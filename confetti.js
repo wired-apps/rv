7bb47013bef0911e0cec177f5f46f4917446e4be
wired-apps <135077010+wired-apps@users.noreply.github.com>
2024-03-20 11:03:15 +0530

    Add files via upload

---
+++ confetti.js
@@ -0,0 +1,9 @@
+import confetti from 'https://cdn.skypack.dev/canvas-confetti';
+
+
+      
+
+//window.addEventListener("message", makeConfetti)
+window.addEventListener("message",function(){
+    console.log('INSIDE');
+})
